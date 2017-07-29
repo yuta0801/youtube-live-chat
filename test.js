56dd38478bca8be9afce43787822f5914b5e6fc4
yuta <yuta20030801@icloud.com>
2017-07-30 03:23:37 +0900

    add test.js

---
+++ test.js
@@ -0,0 +1,16 @@
+const YouTube = require('./main.js');
+
+const yt = new YouTube('channelID', 'APIKEY');
+
+yt.on('ready', () => {
+	console.log('ready!');
+	yt.listen(1000);
+});
+
+yt.on('chat', json => {
+	console.log(json.snippet.displayMessage);
+});
+
+yt.on('error', err => {
+	console.log('err', err);
+});
