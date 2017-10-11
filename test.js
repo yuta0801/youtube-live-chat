a9d0c9b9b88a3d9d35f2df3799ef58d67cae37ed
yuta0801 <yuta20030801@icloud.com>
2017-10-12 00:54:00 +0900

    Minor fixes
    - Move files
    - Fix test.js according to ESLint

---
+++ test.js
@@ -0,0 +1,16 @@
+const YouTube = require('index.js')
+
+const yt = new YouTube('channelID', 'APIKEY')
+
+yt.on('ready', () => {
+  console.log('ready!')
+  yt.listen(1000)
+})
+
+yt.on('chat', data => {
+  console.log(data.snippet.displayMessage)
+})
+
+yt.on('error', error => {
+  console.log(error)
+})
