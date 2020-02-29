eee7a930a6b3adf6741df919aef2633a5e3643f6
yuta0801 <yuta20030801@icloud.com>
2020-02-29 13:26:50 +0900

    format code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const request = require('request')
-const {EventEmitter} = require('events')
+const { EventEmitter } = require('events')
 
 /**
  * The main hub for acquire live chat with the YouTube Date API.
@@ -18,18 +18,18 @@ class YouTube extends EventEmitter {
   }
 
   async getLive() {
-    const url = 'https://www.googleapis.com/youtube/v3/search'+
-      '?eventType=live'+
-      '&part=id'+
-      `&channelId=${this.id}`+
-      '&type=video'+
+    const url =
+      'https://www.googleapis.com/youtube/v3/search' +
+      '?eventType=live' +
+      '&part=id' +
+      `&channelId=${this.id}` +
+      '&type=video' +
       `&key=${this.key}`
-    const data = await this.request(url);
-    if (!data.items.length)
-      this.emit('error', 'Can not find live.')
+    const data = await this.request(url)
+    if (!data.items.length) this.emit('error', 'Can not find live.')
     else {
       this.liveIds = []
-      for(let item in data.items) {
+      for (let item in data.items) {
         this.liveIds.push(data.items[item].id.videoId)
       }
       this.getChatIds()
@@ -39,19 +39,19 @@ class YouTube extends EventEmitter {
   async getChatIds() {
     if (!this.liveIds) return this.emit('error', 'Live ids are not valid.')
     this.chatIds = []
-    for(let id in this.liveIds) {
-      const url = 'https://www.googleapis.com/youtube/v3/videos'+
-      '?part=liveStreamingDetails'+
-      `&id=${this.liveIds[id]}`+
-      `&key=${this.key}`
+    for (let id in this.liveIds) {
+      const url =
+        'https://www.googleapis.com/youtube/v3/videos' +
+        '?part=liveStreamingDetails' +
+        `&id=${this.liveIds[id]}` +
+        `&key=${this.key}`
       const data = await this.request(url)
-      if (!data.items.length)
-          this.emit('error', `Can not find chat for stream ${this.liveIds[id]}`)
+      if (!data.items.length) this.emit('error', `Can not find chat for stream ${this.liveIds[id]}`)
       else {
         this.chatIds.push(data.items[0].liveStreamingDetails.activeLiveChatId)
       }
     }
-    if(this.chatIds.length) this.emit('ready')
+    if (this.chatIds.length) this.emit('ready')
   }
 
   /**
@@ -61,30 +61,31 @@ class YouTube extends EventEmitter {
    */
   async getChats() {
     if (!this.chatIds) return this.emit('error', 'Chat id is invalid.')
-    for(let chat in this.chatIds) {
-      const url = 'https://www.googleapis.com/youtube/v3/liveChat/messages'+
-      `?liveChatId=${this.chatIds[chat]}`+
-      '&part=id,snippet,authorDetails'+
-      '&maxResults=2000'+
-      `&key=${this.key}`
+    for (let chat in this.chatIds) {
+      const url =
+        'https://www.googleapis.com/youtube/v3/liveChat/messages' +
+        `?liveChatId=${this.chatIds[chat]}` +
+        '&part=id,snippet,authorDetails' +
+        '&maxResults=2000' +
+        `&key=${this.key}`
       this.emit('json', await this.request(url))
     }
   }
 
   request(url) {
     return new Promise(resolve => {
-      request({
-        url: url,
-        method: 'GET',
-        json: true,
-      }, (error, response, data) => {
-        if (error)
-          this.emit('error', error)
-        else if (response.statusCode !== 200)
-          this.emit('error', data)
-        else
-          resolve(data)
-      })
+      request(
+        {
+          url: url,
+          method: 'GET',
+          json: true,
+        },
+        (error, response, data) => {
+          if (error) this.emit('error', error)
+          else if (response.statusCode !== 200) this.emit('error', data)
+          else resolve(data)
+        }
+      )
     })
   }
 
@@ -94,7 +95,8 @@ class YouTube extends EventEmitter {
    * @fires YouTube#message
    */
   listen(delay) {
-    let lastRead = 0, time = 0
+    let lastRead = 0,
+      time = 0
     this.interval = setInterval(() => this.getChats(), delay)
     this.on('json', data => {
       for (const item of data.items) {
@@ -102,11 +104,11 @@ class YouTube extends EventEmitter {
         if (lastRead < time) {
           lastRead = time
           /**
-          * Emitted whenever a new message is recepted.
-          * See {@link https://developers.google.com/youtube/v3/live/docs/liveChatMessages#resource|docs}
-          * @event YouTube#message
-          * @type {object}
-          */
+           * Emitted whenever a new message is recepted.
+           * See {@link https://developers.google.com/youtube/v3/live/docs/liveChatMessages#resource|docs}
+           * @event YouTube#message
+           * @type {object}
+           */
           this.emit('message', item)
         }
       }
