acca4855c1336a5fc9a9169143770ed39f6e8a07
yuta0801 <yuta20030801@icloud.com>
2020-03-01 12:48:49 +0900

    prevent endless test

--- test.js
+++ test.js
@@ -22,5 +22,9 @@ setTimeout(() => {
   setTimeout(() => {
     chat.restart()
     console.log('--- restartted listening ---')
+    setTimeout(() => {
+      chat.stop()
+      console.log('--- stopped listening ---')
+    }, 3000)
   }, 3000)
 }, 5000)
